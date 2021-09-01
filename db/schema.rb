41b760315b80bbffd09f25e8cdaf3ab495ef025b
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-09-01 15:51:08 +0500

    Create Result model and migrate it to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_103638) do
+ActiveRecord::Schema.define(version: 2021_09_01_104625) do
+
+  create_table "results", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "writing_score"
+    t.string "decimal"
+    t.decimal "speaking_score"
+    t.integer "reading_correct_answer"
+    t.decimal "reading_score"
+    t.integer "listening_correct_answer"
+    t.decimal "listening_score"
+    t.date "passed_date"
+    t.decimal "overall_score"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_results_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_09_01_103638) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "results", "users"
 end
