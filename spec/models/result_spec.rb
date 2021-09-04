a6f794be31f136baeea7739026238d89740b9120
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-09-04 13:19:17 +0500

    Test result model

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,18 +1,20 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# # Test suite for the Result model
-# RSpec.describe Result, type: :model do
-#   # Association test
-#   # ensure an item record belongs to a single result record
-#   it { should belong_to(:user) }
-#   # Validation test
-#   # ensure columns are present before saving
-#   it { should validate_presence_of(:writing_score) }
-#   it { should validate_presence_of(:speaking_score) }
-#   it { should validate_presence_of(:reading_correct_answer) }
-#   it { should validate_presence_of(:reading_score) }
-#   it { should validate_presence_of(:listening_correct_answer) }
-#   it { should validate_presence_of(:listening_score) }
-#   it { should validate_presence_of(:passed_date) }
-#   it { should validate_presence_of(:overall_score) }
-# end
+RSpec.describe Result, type: :model do
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:writing_score) }
+  it { should validate_numericality_of(:writing_score) }
+  it { should validate_presence_of(:speaking_score) }
+  it { should validate_numericality_of(:speaking_score) }
+  it { should validate_presence_of(:reading_correct_answer) }
+  it { should validate_numericality_of(:reading_correct_answer) }
+  it { should validate_presence_of(:reading_score) }
+  it { should validate_numericality_of(:reading_score) }
+  it { should validate_presence_of(:listening_correct_answer) }
+  it { should validate_numericality_of(:listening_correct_answer) }
+  it { should validate_presence_of(:listening_score) }
+  it { should validate_numericality_of(:listening_score) }
+  it { should validate_presence_of(:passed_date) }
+  it { should validate_presence_of(:overall_score) }
+  it { should validate_numericality_of(:overall_score) }
+end
