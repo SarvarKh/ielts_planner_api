11d0de25d5a7faf38ebc372fce07e213c0be8993
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-09-14 19:00:09 +0500

    Add users show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+  get 'show', to: 'users#show'
 
   resources :results
 end
